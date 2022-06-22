42bfebffe21943d66d1a7c0c27b4de1faa2c47f3
Durza42 <76533796+Durza42@users.noreply.github.com>
2022-06-22 18:21:39 +0200

    basic commit

---
+++ win_geoplatform/win_makefile.sh
@@ -0,0 +1,1 @@
+x86_64-w64-mingw32-gcc  geoplatform.c    -o geoplatform.exe  -I/home/durza42/programing/includes -L/home/durza42/programing/libs  `sdl2-config --libs` -lSDL2_image -lSDL2_mixer -lSDL2_ttf
